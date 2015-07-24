a56432ce25afc794e49ee3d4a2dcf19df5ff3f3b
frenchmike <thefrenchmanp@hotmail.co.uk>
2015-07-25 00:59:11 +0100

    First check in
    Files auto-created from VS2013 - MVC5, Bootstrap, Angular Js

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ButtyOrdering.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ButtyOrdering.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
